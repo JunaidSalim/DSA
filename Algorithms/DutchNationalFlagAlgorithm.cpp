978b952d32fd0f245553432cddea914f07ed6c56
Junaid Saleem <junaidsaleem986@gmail.com>
2024-07-04 09:40:10 +0500

    Update DutchNationalFlagAlgorithm.cpp

--- Algorithms/DutchNationalFlagAlgorithm.cpp
+++ Algorithms/DutchNationalFlagAlgorithm.cpp
@@ -1,27 +1,23 @@
-class Solution
+void sortColors(vector<int> &nums)
 {
-public:
-    void sortColors(vector<int> &nums)
+    int n = nums.size();
+    int low = 0, mid = 0, high = n - 1;
+    while (mid <= high)
     {
-        int n = nums.size();
-        int low = 0, mid = 0, high = n - 1;
-        while (mid <= high)
+        if (nums[mid] == 0)
         {
-            if (nums[mid] == 0)
-            {
-                swap(nums[mid], nums[low]);
-                low++;
-                mid++;
-            }
-            else if (nums[mid] == 1)
-            {
-                mid++;
-            }
-            else
-            {
-                swap(nums[mid], nums[high]);
-                high--;
-            }
+            swap(nums[mid], nums[low]);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(nums[mid], nums[high]);
+            high--;
         }
     }
-};
+}
